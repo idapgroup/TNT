e2366cf3517e796b83f8f060f51bde8b2599a68d
Jeketos <eugene.kotsogub@idapgroup.com>
2019-05-20 17:27:27 +0300

    Add READ_EXTERNAL_STORAGE permission to lib

--- sample/src/main/java/com/idapgroup/jet/MainActivity.kt
+++ sample/src/main/java/com/idapgroup/jet/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.idapgroup.jet
 
+import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -8,6 +9,10 @@ import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.idapgroup.tnt.pickImageFromGallery
 import kotlinx.android.synthetic.main.screen_sample.*
 
@@ -37,6 +42,27 @@ class SampleFragment : Fragment() {
 
     fun onTaken(uri: Uri) {
         Exception().printStackTrace()
-        Glide.with(imageView).load(uri).into(imageView)
+        Glide.with(imageView).load(uri).addListener(object : RequestListener<Drawable>{
+            override fun onLoadFailed(
+                e: GlideException?,
+                model: Any?,
+                target: Target<Drawable>?,
+                isFirstResource: Boolean
+            ): Boolean {
+                e?.printStackTrace()
+                return false
+            }
+
+            override fun onResourceReady(
+                resource: Drawable?,
+                model: Any?,
+                target: Target<Drawable>?,
+                dataSource: DataSource?,
+                isFirstResource: Boolean
+            ): Boolean {
+                return false
+            }
+
+        }).into(imageView)
     }
 }
